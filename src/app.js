378e66f6758702b60ebbc294f241529711135880
Sander Sierra <sierrasander29@gmail.com>
2023-02-27 23:42:54 -0500

    Setup app.js

--- src/app.js
+++ src/app.js
@@ -6,23 +6,38 @@ import cors from "cors"
 import swagger from "swagger-ui-express"
 import docs from "../docs/index.js"
 
+import passport from "passport"
+import session from "express-session"
+
 import handleRoutes from "./routes/index.js"
+
+import "./db/index.js"
 
 const pino = pinohttp({
     transport: {
         target: 'pino-http-print',
         options: {
-            destination: 1,
-            all: true,
             translateTime: true
         }
     }
 })
+
 const app = express()
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use(pino)
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false})
+    //cookie: { secure: true }}
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+import "./config_auth.js"
+
 app.use('/api-docs', swagger.serve, swagger.setup(docs));
 
 handleRoutes(app)
